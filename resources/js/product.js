a5a24dd609ecdb3783ae0daef39d68d10da95e30
Quach Dai Nhu Son <nhuson35a@gmail.com>
2023-08-20 21:22:14 +0700

    Product Detail

--- resources/js/product.js
+++ resources/js/product.js
@@ -6,5 +6,18 @@ $ = function(id) {
       $(id).style.display ='block';
   }
   var hide = function(id) {
-      $(id).style.display ='none';
+      $(id).style.display ='block';
   }
+
+
+    const sizeButtons = document.querySelectorAll('.size-button');
+    const selectedSize = document.querySelector('.selected-size');
+
+    sizeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+        const size = button.getAttribute('data-size');
+        selectedSize.textContent = `Selected size: ${size}`;
+        });
+    });
+
+
