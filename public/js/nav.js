880db8738a9beed0608032ddd2a4ea16d0acda1d
Quach Dai Nhu Son <nhuson35a@gmail.com>
2023-09-18 17:01:14 +0700

    search bar

--- public/js/nav.js
+++ public/js/nav.js
@@ -20,3 +20,12 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header"); 
     header.classList.toggle("sticky", window.scrollY > 0); 
 })
+
+function toggleSearchBar() {
+    var searchBar = document.getElementById('search_bar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
+    }
+}
