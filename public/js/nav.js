8150885555c7fd6799d84f5b5ef39f002b13afa7
Dinh Nam <namdinh2k03@gmail.com>
2023-10-04 20:52:19 +0700

    nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,25 +1,14 @@
-// var query = document.getElementById('navbar__sticky').style;
-// window.addEventListener('scroll',()=>{
-//     const scrolled = window.scrollY;
-//     if(scrolled >=5){
-//         query.display = "block";
-//         query.position = "fixed";
-//         query.top = "0";
-//        query.opacity = "1"
 
-//     }
-//     else{
-//        query.display = "none";
-//        query.position = "static";
-//        query.opacity = "0";
-//        query.transition ="all 2s"
-//     }
-// });
 
 window.addEventListener("scroll", function(){ 
     var header = document.querySelector("header"); 
     header.classList.toggle("sticky", window.scrollY > 0); 
 })
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active')
+}
 
 function toggleSearchBar() {
     var searchBar = document.getElementById('search_bar');
